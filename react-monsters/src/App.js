fb290ab1f8abba52df6bdf507319ddbdd4cc668c
AHTOH2001 <antonlapytko@gmail.com>
2021-07-15 21:16:02 +0300

    Fourth day report

---
+++ react-monsters/src/App.js
@@ -0,0 +1,30 @@
+import { Component } from 'react'
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();    
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response  => response.json())
+    .then(users => this.setState({monsters: users}));      
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name }</h1>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
